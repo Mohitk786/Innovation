ade8970471e123969ad13e35692d73abfd7d73f4
Mohitk786 <118011496+Mohitk786@users.noreply.github.com>
2024-04-23 20:34:43 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,19 +10,18 @@ const Home = () => {
     <div>
       <div 
         className='flex flex-col w-full items-center h-auto'>
-          <div  className="w-[80%] flex flex-col gap-20 items-center ">
-          <div className="min-w-full lg:max-h-[450px]  flex justify-center">
+             <div  className="w-[80%] flex flex-col gap-20 items-center  ">
+          <div 
+            className="flex justify-center scale-x-150 scale-y-150 items-center mt-20 md:scale-110 lg:m-10 lg:scale-100 shadow-black  shadow-2xl">
             <ReactPlayer
               loop={true}
               controls={false}
               playing={true}
               muted={true}
-              width={'1350px'}
-              height={'450px'}
+              width={"700"}
+              height={'700'}
               url={"/assets/video.mp4"}
-              className="min-w-[1250px] h-[650px]"
-            
-            />
+            /> 
           </div>
           
           <Carousel cardData={cardData}/>
